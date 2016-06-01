c08fecbf22815fc2e633d94d078818555b80f6d4
Revill1 <adamredlinski@gmail.com>
2016-06-01 15:51:13 +0200

    added clases

---
+++ SalesTaxesUSA/SalesTaxesUSA/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTaxesUSA
+{
+    class Product
+    {
+        int id;
+        String name;
+        float basePrice;
+        float endPrice;
+        ProductCategory productCategory;
+
+    }
+}
